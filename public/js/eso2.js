4bd1940ac4117fc7499174de074178d3cf03a436
Mustafa Doğuş <mustafadogus45@gmail.com>
2020-10-22 22:41:00 +0300

    Soruyu Dinle düğmesi eklendi

--- public/js/eso2.js
+++ public/js/eso2.js
@@ -343,3 +343,46 @@ $(function () {
         document.getElementById('finish-span').focus();
     });
 });
+
+//Audio
+$(function() {
+    $("#listenQuestion").click(function (e) {
+        var audio = document.getElementById("qAudio");
+        var isPlaying = false;
+        
+        
+        
+        if (this.className == "is-playing") {
+            $("#listenQuestion").attr("class", "");
+            $("#listenQuestion").html("Soruyu Dinle");
+            audio.pause();
+        } else {
+            $("#listenQuestion").attr("class", "is-playing");
+            $("#listenQuestion").html("Durdur");
+            audio.play();
+        }
+        /*
+        if (isPlaying) {
+            $("#listenQuestion").html("Soruyu Dinle");
+            audio.pause();
+        } else {
+            $("#listenQuestion").html("Durdur");
+            audio.play();
+        }*/
+        audio.onplaying = function() {
+            //isPlaying = true;
+            $("#listenQuestion").attr("class", "is-playing");
+            $("#listenQuestion").html("Durdur");
+        }
+        audio.onpause = function() {
+            //isPlaying = false;
+            $("#listenQuestion").attr("class", "");
+            $("#listenQuestion").html("Soruyu Dinle");
+        }
+        audio.onended = function() {
+            //isPlaying = false;
+            $("#listenQuestion").attr("class", "");
+            $("#listenQuestion").html("Soruyu Dinle");
+        }
+    });
+});
