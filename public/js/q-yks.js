6b6244904f4e422db4b1f52ba2db9c25ce84546a
mustafa@gmail.com <mustafa@gmail.com>
2021-02-06 19:02:04 +0300

    Added yks

--- public/js/q-yks.js
+++ public/js/q-yks.js
@@ -5,8 +5,8 @@ var i = 0;
 questionArr[++i] = {
     id: i,
     //1
-	//text: '<p class="question-text"></p><p class="question-stem"><span style="text-decoration:underline;"></span></p>',
-    text: '<p class="question-text">Osmanlı coğrafyasına yolu düşen seyyahların tamamı; devletin büyüklüğünü, halkın medeni hayat şartlarını, şehirlerin güzelliğini, kurum ve kuruluşların intizamını öve öve bitiremezler. Kusursuz işleyen bu sistem; toplamı 15 milyon kilometrekare tutan bir devleti yaşatıyor, içinde 12 eyalet barındırıyordu.</p><p class="question-stem">Bu parçanın anlatımıyla ilgili olarak aşağıdakilerden hangisi <span style="text-decoration:underline;">söylenemez</span>?</p>',
+	//text: '<p class="question-text"></p><p class="question-stem"><u></u></p>',
+    text: '<p class="question-text">Osmanlı coğrafyasına yolu düşen seyyahların tamamı; devletin büyüklüğünü, halkın medeni hayat şartlarını, şehirlerin güzelliğini, kurum ve kuruluşların intizamını öve öve bitiremezler. Kusursuz işleyen bu sistem; toplamı 15 milyon kilometrekare tutan bir devleti yaşatıyor, içinde 12 eyalet barındırıyordu.</p><p class="question-stem">Bu parçanın anlatımıyla ilgili olarak aşağıdakilerden hangisi <u>söylenemez</u>?</p>',
     o1Text: 'İkilemelere yer verilmiştir.',
     o2Text: 'Açıklama yapılmıştır.',
     o3Text: 'Nicel verilerden yararlanılmıştır.',
@@ -14,132 +14,132 @@ questionArr[++i] = {
     o5Text: 'Örneklerden yararlanılmıştır.',
     answer: undefined,
     marked: false,
-    qAudioSrc: '/test/audio/ekpss/q1.mp3',
-    oAudioSrc: '/test/audio/ekpss/o1.mp3'
+    qAudioSrc: '/test/audio/yks/q1.mp3',
+    oAudioSrc: '/test/audio/yks/o1.mp3'
 };
 questionArr[++i] = {
     id: i,
     //2
-	text: '<p class="question-text">Eski Mısırlılar ve Yunanlılar tarafından keşfedilen ve doğada en belirgin örneklerine insan vücudunda, ağaç dallarında ve küçücük deniz kabuklarında rastlanılan “altın oran”; araştırmacılar tarafından bir dikdörtgenin boyunun enine olan “en estetik” oranı olarak tanımlanmaktadır.</p><p class="question-stem">Bu cümlede aşağıdaki ses olaylarından hangisi <u>yoktur</u>?</p>',
-    o1Text: 'Ünlü düşmesi',
-    o2Text: 'Ünsüz benzeşmesi',
-    o3Text: 'Ünsüz yumuşaması',
-    o4Text: 'Ünlü daralması',
-    o5Text: 'Ünsüz düşmesi',
+	text: '<p class="question-text">Hemingway gibi bir yazarın söyledikleri kadar söylemedikleri de önem taşır. Yazılarındaki yoğunluğu hem kelimeleriyle hem de sessizliğiyle sağlar. Hemingway\'in kendi deyimiyle buz dağına benzettiği yazıları, ilk bakışta kendisini kolayca ele vermez.</p><p class="question-stem">Bu parçaya göre Hemingway\'in yazılarındaki en belirgin özellik aşağıdakilerden hangisidir?</p>',
+    o1Text: 'Özgün bir bakış açısı taşıması',
+    o2Text: 'Hayal ögelerinden yararlanması',
+    o3Text: 'Anlam derinliğine sahip olması',
+    o4Text: 'Üretimi için uzun zaman gerektirmesi',
+    o5Text: 'Okuyucunun ilgi alanına girmesi',
     answer: undefined,
     marked: false,
-    qAudioSrc: '/test/audio/ekpss/q2.mp3',
-    oAudioSrc: '/test/audio/ekpss/o2.mp3'
+    qAudioSrc: '/test/audio/yks/q2.mp3',
+    oAudioSrc: '/test/audio/yks/o2.mp3'
 };
 questionArr[++i] = {
     id: i,
     //3
-	text: '<p class="question-stem">Aşağıdakilerin hangisindeki açıklama, parantez içindeki deyim ile anlamca <u>uyuşmamaktadır</u>?</p>',
-    o1Text: 'İçinden çıkılması güç bir durumda kalmak. (Açmaza düşmek)',
-    o2Text: 'Çok iyi bilmek, kesin olarak bilmek. (Adı gibi bilmek)',
-    o3Text: 'Bir işin istediği gibi olması için uygun zemin hazırlamak. (Yolunu yapmak)',
-    o4Text: 'Durup dinlendirmeden çalıştırmak. (Soluk aldırmamak)',
-    o5Text: 'Üzüntü ile derin bir nefes almak. (Göğüs germek)',
+	text: '<p class="question-text">Yeni bir operanın temsili sırasında C. W. Gluck zaman zaman koltuğundan kalkar, yavaşça eğilir, sonra tekrar otururmuş. Yanındaki arkadaşı fısıldamış: “Üstat, neden böyle kalkıp oturuyorsunuz?” Gluck yanıtlamış: “Yapıtta geçen eski tanıdık melodileri selamlıyorum.”</p><p class="question-stem">Bu parçaya göre Gluck, davranışıyla aşağıdakilerden hangisini göstermek istemektedir?</p>',
+    o1Text: 'Klasikleşen ve sevdiği eserlere duyduğu saygıyı gizlemek',
+    o2Text: 'Genç bestekârları daha fazla eser vermeye teşvik etmek',
+    o3Text: 'Sanatın sürekliliğini ve gelenekten beslendiğini anlatmak',
+    o4Text: 'Yeni eserlerdeki özgün ve modern tarzlara dikkat çekmek',
+    o5Text: 'Eserin içeriğinin ve icrasının ayrı şeyler olduğunu göstermek',
     answer: undefined,
     marked: false,
-    qAudioSrc: '/test/audio/ekpss/q3.mp3',
-    oAudioSrc: '/test/audio/ekpss/o3.mp3'
+    qAudioSrc: '/test/audio/yks/q3.mp3',
+    oAudioSrc: '/test/audio/yks/o3.mp3'
 };
 questionArr[++i] = {
     id: i,
     //4
-	text: '<p class="question-text">(1) Bu hikâyede güven içinde yaşamanın her zaman özgürlük getirmeyeceği fikri vurgulanır. (2) Kahramanın Almanya\'ya kaçarak seçtiği sürgün hayatı, zindanda yaşamaktan farksızdır. (3) Çünkü orada geçen her dakika, zaman öldürmektir. (4) Halbuki ömür, zamanı hayata çevirmeye yönelik yaşayınca anlam kazanır. (5) Fakat işin tuhafı doğumla başlayan ömrün bir hayata dönüşmesi de zamanın ölümü demektir.</p><p class="question-stem">Bu parçada numaralanmış cümlelerin hangisinde karşıt anlamlı sözcükler bir arada kullanılmıştır?</p>',
-    o1Text: '1',
-    o2Text: '2',
-    o3Text: '3',
-    o4Text: '4',
-    o5Text: '5',
+	text: '<p class="question-stem">Aşağıdakilerden hangisinin bir masalın döşeme bölümünden alındığı söylenebilir?</p>',
+    o1Text: 'Pireler berber iken develer tellal iken...',
+    o2Text: 'Onlar ermiş muradına, biz çıkalım kerevetine.',
+    o3Text: 'Uzak memleketin birinde bir padişah yaşarmış.',
+    o4Text: 'Padişah işini göredursun, biz gelelim Keloğlan’a.',
+    o5Text: 'Sislerin arasından ansızın tek gözlü bir dev belirivermiş.',
     answer: undefined,
     marked: false,
-    qAudioSrc: '/test/audio/ekpss/q4.mp3',
-    oAudioSrc: '/test/audio/ekpss/o4.mp3'
+    qAudioSrc: '/test/audio/yks/q4.mp3',
+    oAudioSrc: '/test/audio/yks/o4.mp3'
 };
 questionArr[++i] = {
     id: i,
     //5
-	text: '<p class="question-text">Cennet kuşları, Avustralya\'nın doğusundan Yeni Gine\'ye uzanan bölgede ve çevre adalarda bulunur. Alçak bataklık arazilerden, deniz düzeyinden üç bin beş yüz metre yukarıdaki bulut ormanlarına kadar çok farklı yüksekliklerde yaşar. Yiyecekleri bol, yaşam alanları çeşitli, avcıları da az olan bu kuşlar, evrim geçirerek çeşitlenmiştir. Boyu on altı santimetreden yüz yirmi beş santimetreye kadar ulaşabilen otuz dokuz türü vardır. Tüylerinin rengi türüne göre geniş bir yelpaze içinde değişiklik gösterir.</p><p class="question-stem">Bu parçada söz edilen cennet kuşlarıyla ilgili olarak aşağıdakilerden hangisine <u>değinilmemiştir</u>?</p>',
-    o1Text: 'Yaşam alanlarının bazı niteliklerine',
-    o2Text: 'Türlerinin belirli özelliklerine',
-    o3Text: 'Zamanla değişime uğradığına',
-    o4Text: 'Yiyeceklerini avlanarak elde ettiklerine',
-    o5Text: 'Beslenme olanaklarının yeterliliğine',
+	text: '<p class="question-text">...</p><p class="question-text">Yine günlerden bir gün Ay Kağan’ın gözü parladı. Doğum sancıları başladı ve bir erkek çocuğu oldu.</p><p class="question-text">Bu çocuğun yüzü gök, ağzı ateş gibi kızıl, gözleri ela, saçları ve kaşları kara idi. Perilerden daha güzeldi.</p><p class="question-text">Bu çocuk anasının göğsünden ilk sütü emdi ve bir daha emmedi. Çiğ et ve çorba istedi. Dile gelmeye başladı. Kırk gün sonra büyüdü, yürüdü ve oynadı. Ayağı öküz ayağı gibi, beli kurt beli gibi...</p><p class="question-stem">Oğuz Kağan Destanından alınan bu metinle ilgili aşağıdakilerden hangisi <u>söylenemez</u>?</p>',
+    o1Text: 'Kahramanı ruhsal derinlikleriyle tasvir edilmiştir.',
+    o2Text: 'Gerçek ve hayalî unsurlar bir arada verilmiştir.',
+    o3Text: 'Tasvirlerde tabiat unsurlarına başvurulmuştur.',
+    o4Text: 'Zamanla ilgili unsurlar gerçeküstüdür.',
+    o5Text: 'Kahramanı olağanüstü özellikler taşımaktadır.',
     answer: undefined,
     marked: false,
-    qAudioSrc: '/test/audio/ekpss/q5.mp3',
-    oAudioSrc: '/test/audio/ekpss/o5.mp3'
+    qAudioSrc: '/test/audio/yks/q5.mp3',
+    oAudioSrc: '/test/audio/yks/o5.mp3'
 };
 questionArr[++i] = {
     id: i,
     //6
-	text: '<p class="question-text">Seyit, bir Türk topluluğuyla ilgili yaptığı araştırmada şu bilgilere ulaşmıştır:</p><ul><li><p class="question-text">XI. yüzyılda Bizans ile iyi ilişkiler kurarak Bizans ordusunda paralı askerlik yapmışlardır.</p></li><li><p class="question-text">Malazgirt Savaşı\'nda Selçuklu Devleti\'nin tarafına geçmişlerdir.</p></li><li><p class="question-text">Çaka Bey ile birlikte İstanbul kuşatmasına katılmışlardır.</p></li></ul><p class="question-stem">Buna göre Seyit, aşağıdaki Türk topluluklarından hangisini araştırmıştır?</p>',
-    o1Text: 'Bulgarlar',
-    o2Text: 'Avarlar',
-    o3Text: 'Türgişler',
-    o4Text: 'Peçenekler',
-    o5Text: 'Kıpçaklar',
+	text: '<p class="question-stem">Aşağıdakilerden hangisi İslamiyet öncesi Türk devletlerinde geçerli olan egemenlik sembol ve göstergelerinden biri <u>değildir</u>?</p>',
+    o1Text: 'Hutbe okutmak',
+    o2Text: 'Siyasi antlaşmalar yapmak',
+    o3Text: 'Para bastırmak',
+    o4Text: 'Kurultaya başkanlık yapmak',
+    o5Text: 'Yabancı elçileri kabul etmek',
     answer: undefined,
     marked: false,
-    qAudioSrc: '/test/audio/ekpss/q6.mp3',
-    oAudioSrc: '/test/audio/ekpss/o6.mp3'
+    qAudioSrc: '/test/audio/yks/q6.mp3',
+    oAudioSrc: '/test/audio/yks/o6.mp3'
 };
 questionArr[++i] = {
     id: i,
     //7
-	text: '<p class="question-stem">Doğu vilayetlerinin Ermenilere verilmesini önlemek ve Millî Mücadele\'ye destek vermek amacıyla Erzurum\'da çıkarılan gazete aşağıdakilerden hangisidir?</p>',
-    o1Text: 'İrade-i Milliye',
-    o2Text: 'Hakimiyet-i Milliye',
-    o3Text: 'Albayrak',
-    o4Text: 'Açıksöz',
-    o5Text: 'Gaye-i Milliye',
+	text: '<p class="question-stem">Aşağıdakilerden hangisi Mısır Uygarlığı\'nın özelliklerinden biri <u>değildir</u>?</p>',
+    o1Text: 'Hiyeroglif adı verilen resim yazısını kullanmışlardır.',
+    o2Text: 'Nom adı verilen şehir devletleri kurmuşlardır.',
+    o3Text: 'Hititlerle Kadeş Antlaşması\'nı yapmışlardır.',
+    o4Text: 'Sınıfsız bir toplum yapısı oluşturmuşlardır.',
+    o5Text: 'Ölümden sonraki yaşama inanmışlardır.',
     answer: undefined,
     marked: false,
-    qAudioSrc: '/test/audio/ekpss/q7.mp3',
-    oAudioSrc: '/test/audio/ekpss/o7.mp3'
+    qAudioSrc: '/test/audio/yks/q7.mp3',
+    oAudioSrc: '/test/audio/yks/o7.mp3'
 };
 questionArr[++i] = {
     id: i,
     //8
-	text: '<p class="question-stem">Aşağıdakilerden hangisi deniz suyundan tuz elde edilen merkezlerden biridir?</p>',
-    o1Text: 'Çamaltı (İzmir)',
-    o2Text: 'Fatsa (Ordu)',
-    o3Text: 'Silifke (Mersin)',
-    o4Text: 'Çınarcık (Yalova)',
-    o5Text: 'Fethiye (Muğla)',
+	text: '<p class="question-text">Cumhuriyetin ilk yıllarında Türkiye\'nin ekonomik gelişimini araştıran Esra, bir gazetede “Millî Sanayinin Temelini On Yılda Attık” manşetini okumuştur.</p><p class="question-stem">Bu gazete manşeti aşağıdaki ilkelerden hangisiyle açıklanabilir?</p>',
+    o1Text: 'Cumhuriyetçilik',
+    o2Text: 'Laiklik',
+    o3Text: 'Halkçılık',
+    o4Text: 'Devletçilik',
+    o5Text: 'İnkılapçılık',
     answer: undefined,
     marked: false,
-    qAudioSrc: '/test/audio/ekpss/q8.mp3',
-    oAudioSrc: '/test/audio/ekpss/o8.mp3'
+    qAudioSrc: '/test/audio/yks/q8.mp3',
+    oAudioSrc: '/test/audio/yks/o8.mp3'
 };
 questionArr[++i] = {
     id: i,
     //9
-	text: '<p class="question-stem">Türkiye Cumhuriyeti Anayasası\'na göre, cumhurbaşkanının yurt dışına çıkarak geçici olarak görevinden ayrılması hâlinde yerine aşağıdakilerden hangisi vekâlet eder?</p>',
-    o1Text: 'Türkiye Büyük Millet Meclisi başkanı',
-    o2Text: 'Genelkurmay başkanı',
-    o3Text: 'İçişleri bakanı',
-    o4Text: 'Anayasa Mahkemesi başkanı',
-    o5Text: 'Başbakan',
+	text: '<p class="question-stem">Aşağıdakilerden hangisinin, Türkiye\'deki kültür turizminin gelişmesine olan katkısı diğerlerine göre <u>daha azdır</u>?</p>',
+    o1Text: 'Farklı dinlere ait tarihî değer taşıyan ibadethanelerin restore edilmesi',
+    o2Text: 'Kırsal hayata ilişkin değerlerin görünür kılınması',
+    o3Text: 'Yeni kayak merkezlerinin açılarak yaygınlaştırılması',
+    o4Text: 'Arkeolojik alanlara ziyaretçi erişiminin kolaylaştırılması',
+    o5Text: 'Etnoğrafya müzelerinin sayısının artırılması',
     answer: undefined,
     marked: false,
-    qAudioSrc: '/test/audio/ekpss/q9.mp3',
-    oAudioSrc: '/test/audio/ekpss/o9.mp3'
+    qAudioSrc: '/test/audio/yks/q9.mp3',
+    oAudioSrc: '/test/audio/yks/o9.mp3'
 };
 questionArr[++i] = {
     id: i,
     //10
-	text: '<p class="question-stem">Peki Peki Anladık, Ele Güne Karşı, Mazeretim Var Asabiyim Ben ve Güllerin İçinden şarkılarıyla bilinen müzik grubu aşağıdakilerden hangisidir?</p>',
-    o1Text: 'Kurtalan Ekspres',
-    o2Text: 'Mazhar-Fuat-Özkan',
-    o3Text: 'Grup Gündoğarken',
-    o4Text: 'Ezginin Günlüğü',
-    o5Text: 'Yeni Türkü',
+	text: '<p class="question-text">İnsan ihtiyaçları doğrultusunda kullanılan doğal kaynakların miktarı ve çeşidi sürekli artmaktadır.  Bu kaynakların kullanılması sonucu ortaya çıkan atıkların bir kısmının geri dönüştürülmesi çeşitli yararlar sağlamaktadır.</p><p class="question-stem">Aşağıdakilerden hangisi bu yararlardan biri <u>değildir</u>?</p>',
+    o1Text: 'Çevreye verilen zararın azaltılması',
+    o2Text: 'Doğal kaynak kullanımının azaltılması',
+    o3Text: 'Enerji tasarrufunun sağlanması',
+    o4Text: 'Ülke ekonomisine olan katkının artması',
+    o5Text: 'Atık depolama alanlarının sayıca artması',
     answer: undefined,
     marked: false,
-    qAudioSrc: '/test/audio/ekpss/q10.mp3',
-    oAudioSrc: '/test/audio/ekpss/o10.mp3'
+    qAudioSrc: '/test/audio/yks/q10.mp3',
+    oAudioSrc: '/test/audio/yks/o10.mp3'
 };
